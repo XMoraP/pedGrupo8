fff9a4814781295078fb1eb67e225625be8c52fa
Salchikiller <caralepenafiel@gmail.com>
2024-04-20 21:44:43 +0200

    test

--- alejandro/cli3.py
+++ alejandro/cli3.py
@@ -1,25 +1,26 @@
+import os
 import sys
 
 def cli3(fifo_name):
     try:
-        # Abre la FIFO para escribir la solicitud al servidor
+        # Open the FIFO for writing
         with open(fifo_name, "w") as fifo:
             fifo.write("Request\n")
 
-        # Abre la FIFO para leer la respuesta del servidor
+        # Open the FIFO for reading
         with open(fifo_name, "r") as fifo:
-            # Lee la respuesta del servidor
+            # Read the response from the server
             server_response = fifo.readline().strip()
 
-            # Muestra la respuesta en la salida estándar
-            print("Respuesta del servidor:", server_response)
+            # Display the response
+            print("Server Response:", server_response)
 
     except Exception as e:
-        print("Error en el cliente:", e)
+        print("Error in the client:", e)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Uso: python cli3.py <fifo_name>")
+        print("Usage: python cli3.py <fifo_name>")
         sys.exit(1)
 
     fifo_name = sys.argv[1]
