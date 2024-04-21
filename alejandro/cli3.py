4de73ca676f62e6cdca7948d2cc98c5ae49ef7a4
Salchikiller <caralepenafiel@gmail.com>
2024-04-21 03:57:05 +0200

    woking ish con dos terminales

--- alejandro/cli3.py
+++ alejandro/cli3.py
@@ -1,1 +1,12 @@
-import os 
+import os
+
+nombre_fifo = '/tmp/ped8'
+
+fifo_fecha_y_hora = os.open(nombre_fifo, os.O_RDONLY)
+
+
+respuesta_serv3 = os.read(fifo_fecha_y_hora, 1024).decode().strip()
+
+print("respuesta del servidor:", respuesta_serv3)
+
+os.close(fifo_fecha_y_hora)
--- alejandro/serv3.py
+++ alejandro/serv3.py
@@ -9,8 +9,8 @@ if not os.path.exists(nombre_fifo):
 
 fifo_fecha_y_hora = os.open(nombre_fifo, os.O_WRONLY)
 
-curret_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+current_time = time.strftime("%Y-%m-%d %H:%M:%S" + "\n", time.localtime())
 
-os.write(fifo_fecha_y_hora, curret_time.encode())
+os.write(fifo_fecha_y_hora , current_time.encode())
 
 os.close(fifo_fecha_y_hora)
