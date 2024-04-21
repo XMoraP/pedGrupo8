2af1fdbe3759d81fe9c8d302a94a8a440d0be96c
Salchikiller <caralepenafiel@gmail.com>
2024-04-21 02:33:56 +0200

    server creado de tiempo

--- alejandro/serv3.py
+++ alejandro/serv3.py
@@ -1,6 +1,16 @@
 import os 
+import time
 
 nombre_fifo = '/tmp/ped8'
 
 if not os.path.exists(nombre_fifo):
     os.mkfifo(nombre_fifo)
+
+
+fifo_fecha_y_hora = os.open(nombre_fifo, os.O_WRONLY)
+
+curret_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+
+os.write(fifo_fecha_y_hora, curret_time.encode())
+
+os.close(fifo_fecha_y_hora)
