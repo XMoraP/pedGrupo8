e7c43c78d588b66b4faa7d93013f8a2ff6c9a3fb
Dulibeth <dulibeth.medinachirinos@usp.ceu.es>
2024-04-29 20:06:57 +0200

    P5 con hora y fecha, sin comando stop

--- dulibeth/p5/cli5.py
+++ dulibeth/p5/cli5.py
@@ -1,5 +1,13 @@
 import socket
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-data = 'Hola soy el cliente'
-client_socket.sendto(data.encode(), ('192.168.127.139', 8000))
+try:
+    data = 'Hola soy el cliente'
+    client_socket.sendto(data.encode(), ('192.168.127.138', 8000))
+    response, server_address = client_socket.recvfrom(1024)
+
+    print(response.decode())
+except KeyboardInterrupt:
+    pass
+finally:
+    client_socket.close()
