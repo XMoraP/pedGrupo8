43a3e3645c7321f826e6a02b85a937e3ee8b5f4d
Dulibeth <dulibeth.medinachirinos@usp.ceu.es>
2024-04-30 00:56:15 +0200

    p5 con ficheros, falta path por  makefile y binario

--- dulibeth/p5-ficheros/serv5.py
+++ dulibeth/p5-ficheros/serv5.py
@@ -1,18 +1,28 @@
 import socket
-from datetime import datetime
+import os
 
-serv_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-serv_socket.bind(('192.168.127.138', 8000))
+host = "192.168.127.139"
+port = 8000
+
+sock_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock_server.bind((host, port))
 print("Servidor udp esperando clientes...")
 
-try:
-    while True:
-            data, client_address = serv_socket.recvfrom(1024)
-            print("Nueva conexión de cliente: ", client_address)
-            current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            response = "Fecha y hora actual: " + current_time
-            serv_socket.sendto(response.encode(), client_address)
-except KeyboardInterrupt:
-    print("Deteniendo el servidor...")
-finally:
-    serv_socket.close()
+while True:
+    data, addr = sock_server.recvfrom(1024)
+    file_path = data.decode("utf-8")
+    print("Conexión de cliente: ", addr)
+    print("Ruta del archivo recibida:", file_path)
+
+    try:
+        with open(file_path, 'rb') as file:
+            while True:
+                file_chunk = file.read(1024)
+                if not file_chunk:
+                    sock_server.sendto(b"", addr)
+                    break
+                sock_server.sendto(file_chunk, addr)
+                print("Fragmento enviado al cliente.")
+            print("Archivo completo enviado al cliente.")  
+    except FileNotFoundError:
+        print("El archivo no existe en la ruta proporcionada.")
