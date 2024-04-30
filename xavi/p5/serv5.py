ec58eccdbe4b8533e18d5bde079919ef909c4d4b
Xavi <xmorap@outlook.es>
2024-04-30 13:35:02 +0000

    fix serv5

--- xavi/p5/serv5.py
+++ xavi/p5/serv5.py
@@ -5,17 +5,17 @@ serv_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 serv_socket.bind(('192.168.164.138', 8008))
 
 while True:
-    data_r, address = serv_socket.recvfrom(256)
+    data_r, address = serv_socket.recvfrom(1024)
     print(address)
     if not data_r:
         break
     file = os.open(data_r.decode('utf-8'), os.O_RDONLY)
     while True: 
-        data_s = os.read(file, 256)
+        data_s = os.read(file, 1024)
         if not data_s:
+            serv_socket.sendto(b"", address)
             break
         serv_socket.sendto(data_s, address)
-        time.sleep(0.1)
     os.close(file)
 
 serv_socket.close()
