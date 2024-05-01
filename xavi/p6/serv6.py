6c024b891d43f538ae2724d99c2db7eefe6d8fa3
Xavier Mora <xavieralexander.moraperaza@usp.ceu.es>
2024-05-01 20:37:59 +0200

    exect server

--- xavi/p6/serv6.py
+++ xavi/p6/serv6.py
@@ -8,19 +8,25 @@ server_socket.listen(3)
 
 stderr = sys.stderr.fileno()
 
-while True:
-    connection, address = server_socket.accept()
-    os.write(stderr, str(address).encode('utf-8')  + b'\n')
-
-    fd = connection.fileno()
-
-    file = os.read(fd, 1024)
-    fd_file = os.open(file, os.O_RDONLY)
+try:
     while True:
-        data = os.read(fd_file, 1024)
-        if not data:
-            break
-        os.write(fd, data)
-    os.close(fd_file)
-    connection.close()
-server_socket.close()
+        connection, address = server_socket.accept()
+        os.write(stderr, str(address).encode('utf-8')  + b'\n')
+
+        fd = connection.fileno()
+
+        file = os.read(fd, 1024)
+        fd_file = os.open(file, os.O_RDONLY)
+        while True:
+            data = os.read(fd_file, 1024)
+            if not data:
+                break
+            os.write(fd, data)
+        os.close(fd_file)
+        connection.close()
+except BrokenPipeError:
+    pass
+except  KeyboardInterrupt:
+    print("Server stopped")
+finally:
+    server_socket.close()
