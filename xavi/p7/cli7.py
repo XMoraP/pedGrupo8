68f2bff84c50eb17bada1337abfb3087a63febf2
Xavier Mora <xavieralexander.moraperaza@usp.ceu.es>
2024-05-10 01:59:42 +0200

    autentificacion funcionando sin encriptacion

--- xavi/p7/cli7.py
+++ xavi/p7/cli7.py
@@ -1,26 +1,42 @@
-import socket, os, sys
+import socket
+import select
+import sys
+import getpass
 
-client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-client_sock.connect(('192.168.164.141', 8008))
+login = sys.argv[1]
 
-user = sys.argv[1]
+cliente_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+cliente_socket.connect(('10.60.59.104', 8888))
+cliente_socket.send(login.encode())
 
-stdout = sys.stdout.fileno()
+passwd = getpass.getpass(prompt="Introduzca su contraseña: ")
+print(f"Contraseña ingresada: {passwd}")
+cliente_socket.send(passwd.encode())
 
-fd = client_sock.fileno()
-os.write(fd, user.encode('utf-8'))
 
-out = False
 
-while not out:
-    chat_output =  os.read(fd, 1024)
-    os.write(stdout, chat_output)
-    chat_input = input('>>>[' + user + ']:')
-    if chat_input.lower() == 'salir':
-        os.write(stdout, b'Saliendo del chat...')
-        out = True
-        os.close(fd)
-    else:
-        os.write(fd, chat_input.encode('utf-8')+b'\n')
+new_user = f'[' + str(login) + ']'
 
+lista_lectura = [cliente_socket, sys.stdin]
+
+def mostrar_prompt():
+    # Imprime el prompt sin nueva línea al final.
+    print(f'[{str(login)}]:', end=' ', flush=True)
+
+while True:
+    lectura, _, _ = select.select(lista_lectura, [], [])
+    for sock in lectura:
+        if sock is cliente_socket:
+            mensaje = cliente_socket.recv(1024).decode()
+            if not mensaje:
+                print("Desconectando...")
+                sys.exit()
+            else: 
+                print(f'\r{mensaje}')
+        else:
+            mostrar_prompt()
+            mensaje = input().strip()
+            cliente_socket.send(mensaje.encode())
+
+cliente_socket.close()
--- xavi/p7/serv7.py
+++ xavi/p7/serv7.py
@@ -1,63 +1,98 @@
-import socket, os, sys, select
+import select
+import socket
+import sys
 
-server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def enviar_mensajes(mensaje, emisor, clientes):
+    for cliente in clientes:
+        if cliente != emisor:
+            try:
+                cliente.send(mensaje)
+            except:
+                cliente.close()
+                clientes.remove(cliente)
 
-server_sock.bind(('0.0.0.0', 8008))
+def detener_servidor():
+    print("Deteniendo el servidor...")
+    mensaje_desconexion = "El servidor se ha detenido. Desconéctese por favor."
+    for cliente in clientes:
+        cliente.send(mensaje_desconexion.encode())
+    serv_socket.close()
+    sys.exit()
 
-stderr = sys.stderr.fileno()
+serv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serv_socket.bind(('0.0.0.0', 8888))
+serv_socket.listen(5)
 
-server_sock.listen(3) 
-os.write(stderr, b'Esperando conexiones\n')
+clientes = []
+lista_lectura = [serv_socket]
 
-fd_server = server_sock.fileno()
+bd_usuarios = {}
+puerto = {}
 
-rlist = [server_sock]
-wlist = []
+def mostrar_prompt(login):
+   return f'[{str(login)}]:'
 
-def login(fd):
-    login = os.read(fd, 1024)
-    new_user = f'[' + str(login) + ']'
-    os.write(stderr, new_user.encode('utf-8'))
-    db_users = ['user']
-    print('no he entrado en el for')
-    for user in db_users:
-        print('he entrado en el for')
-        print(user)
-        print(login.decode('utf-8'))
-        if user == login.decode('utf-8'):
-            return 'Nombre usuario no disponible'            
-        else:
-            db_users.append(str(new_user))
-            user_msg = str(new_user) + 'Se ha unido al chat.\n'
-            print('estoy aqui')
-            return user_msg
+print("Servidor activo." + "\n" + "Esperando clientes...")
+
 while True:
-    ready_read, ready_write, _ = select.select(rlist, wlist, [], None)
+    lectura, _, _ = select.select(lista_lectura + [sys.stdin], [], [])
 
-    for sock in ready_read:
-        if sock is server_sock:
-            client_sock, address = server_sock.accept()
-            os.write(stderr, str(address).encode('utf-8')+b'\n')
-            rlist.append(client_sock)
-            login(server_sock.fileno())
-            os.write()
+    for sock in lectura:
+
+        if sock is serv_socket:
+            cliente_socket, cliente_addr = serv_socket.accept()
+            print("Nueva conexión de cliente ", cliente_addr)
+            user = cliente_socket.recv(1024)
+            my_user = '[' + str(user.decode('utf-8')) + ']'
+            passwd = cliente_socket.recv(1024)
+            my_passwd = passwd.decode('utf-8')
+            prompt = mostrar_prompt(user.decode('utf-8'))
+            cliente_socket.send(prompt.encode())
+
+            if my_user in bd_usuarios:
+
+                if my_passwd != bd_usuarios[my_user]['passwd']:
+                    mensaje_para_cliente = "Contraseña incorrecta"
+                    cliente_socket.send(mensaje_para_cliente.encode())
+                    cliente_socket.close()
+                elif my_user in puerto.values():
+                    mensaje_para_cliente = "Este usuario ya se encuentra en linea."
+                    cliente_socket.send(mensaje_para_cliente.encode())
+                    cliente_socket.close()
+                elif my_passwd == bd_usuarios[my_user]['passwd']:
+                    welcome = '\nBienvenido ' + my_user
+                    puerto[cliente_socket.getpeername()[1]] = my_user
+                    cliente_socket.send(welcome.encode())
+                    print(user.decode('utf-8'))
+                    lista_lectura.append(cliente_socket)
+                    clientes.append(cliente_socket)
+
+            else:
+                ms_para_cliente = '\nBienvenido nuevo usuario'
+                cliente_socket.send(ms_para_cliente.encode())
+                puerto[cliente_socket.getpeername()[1]] = my_user
+                bd_usuarios[my_user] = {'name': my_user, 'passwd': my_passwd}                
+                print(user.decode('utf-8'))
+                print(bd_usuarios)
+                lista_lectura.append(cliente_socket)
+                clientes.append(cliente_socket)
+
+        elif sock is sys.stdin:
+            comando = sys.stdin.readline().strip().lower() 
+            if comando == "stop":
+                detener_servidor()
+
         else:
-            fd = client_sock.fileno()
-            data_chat =os.read(fd, 1024)
-            for sock in ready_write:
-                os.write(sock.fileno(), data_chat)
-    while True: 
-        chat_msg = os.read(fd, 1024)
-        if not chat_msg:
-            break
-        os.write(fd, chat_msg)
-    
+            datos = sock.recv(1024)
+            if datos:
+                valor = puerto[sock.getpeername()[1]]
+                mensaje = f"{valor}: {datos.decode()}"
+                print(mensaje)
+                enviar_mensajes(mensaje.encode(), sock, clientes)
 
-
-    
-
-    
-
-
-
-
+            else:
+                print("Cliente desconectado.")
+                del puerto[sock.getpeername()[1]]
+                lista_lectura.remove(sock)
+                clientes.remove(sock)
+                sock.close()
