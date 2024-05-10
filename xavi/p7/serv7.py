505fa912271e66b20f8b7c0c54806aa627086a28
Xavier Mora <xavieralexander.moraperaza@usp.ceu.es>
2024-05-10 13:00:27 +0200

    fix

--- xavi/p7/serv7.py
+++ xavi/p7/serv7.py
@@ -32,7 +32,7 @@ bd_usuarios = {}
 puerto = {}
 
 def mostrar_prompt(login):
-   return f'[{str(login)}]'
+   return f'[{str(login)}]: '
 
 print("Servidor activo." + "\n" + "Esperando clientes...")
 
@@ -53,7 +53,7 @@ while True:
 
             if my_user in bd_usuarios:
                 
-                if not bcrypt.identical(my_passwd, bd_usuarios[my_user]['passwd']):
+                if not bcrypt.identify(my_passwd, bd_usuarios[my_user]['passwd']):
                     mensaje_para_cliente = "Contraseña incorrecta"
                     cliente_socket.send(mensaje_para_cliente.encode())
                     cliente_socket.close()
@@ -61,7 +61,7 @@ while True:
                     mensaje_para_cliente = "Este usuario ya se encuentra en linea."
                     cliente_socket.send(mensaje_para_cliente.encode())
                     cliente_socket.close()
-                elif bcrypt.identical(my_passwd, bd_usuarios[my_user]['passwd']):
+                elif bcrypt.verify(my_passwd, bd_usuarios[my_user]['passwd']):
                     welcome = '\nBienvenido ' + my_user
                     puerto[cliente_socket.getpeername()[1]] = my_user
                     cliente_socket.send(welcome.encode())
@@ -88,7 +88,7 @@ while True:
             datos = sock.recv(1024)
             if datos:
                 valor = puerto[sock.getpeername()[1]]
-                mensaje = f"{valor}\n {datos.decode()}"
+                mensaje = f"{valor}: {datos.decode()}"
                 print(mensaje)
                 enviar_mensajes(mensaje.encode(), sock, clientes)
 
